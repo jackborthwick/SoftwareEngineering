e491bf0c1d8f47ea6be29906d2c07049d1306a0f
Jack Borthwick <jackmckeanborthwick@gmail.com>
2015-08-28 11:29:13 -0600

    day 5 commit

--- clientForCustomServer.py
+++ clientForCustomServer.py
@@ -3,6 +3,6 @@ import http.client, urllib.parse
 from pprint import pprint
 url = 'localhost:8000'
 connection = http.client.HTTPConnection(url)
-connection.request('POST', "/lets/post/it?definitely=always", "I am changing stuff again") 
+connection.request('POST', "/lets/post/it?definitely=always", "I am changing st,lmn,uff again")
 response = connection.getresponse()
 pprint(response.read())
